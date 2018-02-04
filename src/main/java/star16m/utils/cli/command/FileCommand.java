416d9b92f1303b19c9a42fdc8268284e1f793969
star16m <star16m@gmail.com>
2018-02-04 14:18:25 +0900

    change name

--- src/main/java/star16m/utils/cli/command/FileCommand.java
+++ src/main/java/star16m/utils/cli/command/FileCommand.java
@@ -15,12 +15,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DemoCommand implements CommandLineRunner {
-	private Options options = new Options();
-	private DefaultParser parser = new DefaultParser();
-	private HelpFormatter formatter = new HelpFormatter();
-	@PostConstruct
-	public void init() {
+public class FileCommand implements CommandLineRunner {
+    private Options options = new Options();
+    private DefaultParser parser = new DefaultParser();
+    private HelpFormatter formatter = new HelpFormatter();
+
+    @PostConstruct
+    public void init() {
         options = new Options();
         parser = new DefaultParser();
         formatter = new HelpFormatter();
@@ -28,37 +29,38 @@ public class DemoCommand implements CommandLineRunner {
         options.addOption("h", "help", false, "Print help message.");
         options.addOption("d", "dir", true, "target directory");
         options.addOption("t", "type", true, "Print file type. [d|f]");
-	}
-	private void printHelp() {
-		System.out.println("===================================================");
+    }
+
+    private void printHelp() {
+        System.out.println("===================================================");
         formatter.printHelp("clifind [-t <type[f|d]>]", options);
         System.out.println("===================================================");
-	}
-	@Override
-	public void run(String... args) throws Exception {
-        
-		CommandLine cl = null;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        CommandLine cl = null;
         try {
-        	cl = parser.parse(options, args);
+            cl = parser.parse(options, args);
         } catch (ParseException e) {
-        	printHelp();
-        	System.exit(1);
+            printHelp();
+            System.exit(1);
         }
         if (cl.hasOption("h")) {
-        	printHelp();
+            printHelp();
             return;
         }
-        
+
         String dirString = cl.getOptionValue("d");
         if (!cl.hasOption("d") || StringUtils.isEmpty(dirString)) {
-        	printHelp();
-        	return;
+            printHelp();
+            return;
         }
         String type = cl.getOptionValue("t");
-        
+
         File rootFile = Paths.get(dirString).toFile();
         System.out.println(rootFile.getAbsolutePath());
-        System.out.println(type);
-	}
+    }
 
 }
