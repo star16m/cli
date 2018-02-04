7984dfa06a808d8e10943c91339fc44a8af75fc1
star16m <star16m@gmail.com>
2018-02-04 17:20:43 +0900

    delete import

--- src/main/java/star16m/utils/cli/CliApplication.java
+++ src/main/java/star16m/utils/cli/CliApplication.java
@@ -2,7 +2,6 @@ package star16m.utils.cli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class CliApplication {
