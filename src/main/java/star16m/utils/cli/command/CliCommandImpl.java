164e431b37feb473446c08b376947cd831e1ed83
star16m <star16m@gmail.com>
2018-02-17 11:55:38 +0900

    sadf

--- src/main/java/star16m/utils/cli/command/CliCommandImpl.java
+++ src/main/java/star16m/utils/cli/command/CliCommandImpl.java
@@ -20,23 +20,24 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 public class CliCommandImpl implements CommandLineRunner {
-	@Autowired
-	private ApplicationContext applicationContext;
+    @Autowired
+    private ApplicationContext applicationContext;
     private Options options = new Options();
     private DefaultParser parser = new DefaultParser();
     private HelpFormatter formatter = new HelpFormatter();
 
     private void printHelp() {
-    	printHelp("cli");
+        printHelp("cli");
     }
+
     private void printHelp(String message) {
-    	String banner = "===================================================";
+        String banner = "==================================================";
         formatter.printHelp(message, banner, options, banner);
     }
 
     @Override
     public void run(String... args) throws Exception {
-    	options = new Options();
+        options = new Options();
         parser = new DefaultParser();
         formatter = new HelpFormatter();
         CliCommand command = null;
@@ -44,40 +45,41 @@ public class CliCommandImpl implements CommandLineRunner {
         commandName = args[0];
         String[] commandArguments = Arrays.copyOfRange(args, 1, args.length);
         try {
-        	// 1. find sub command
-        	command = applicationContext.getBean(commandName, CliCommand.class);
-        	
-        	// 2. initialize option as sub command
-        	command.initOption(this.options);
+            // 1. find sub command
+            command = applicationContext.getBean(commandName, CliCommand.class);
+
+            // 2. initialize option as sub command
+            command.initOption(this.options);
         } catch (Throwable e) {
-        	
-        	if (command == null) {
-        		// not found sub command
-        		Map<String, CliCommand> definedCommandMap = applicationContext.getBeansOfType(CliCommand.class);
-        		printHelp(String.format("Please specify command. [%s]", definedCommandMap.keySet().stream().collect(Collectors.joining(", "))));
-        		System.exit(1);
-        	}
-        	
-        	printHelp();
-        	System.exit(1);
+
+            if (command == null) {
+                // not found sub command
+                Map<String, CliCommand> definedCommandMap = applicationContext.getBeansOfType(CliCommand.class);
+                printHelp(String.format("Please specify command. [%s]",
+                        definedCommandMap.keySet().stream().collect(Collectors.joining(", "))));
+                System.exit(1);
+            }
+
+            printHelp();
+            System.exit(1);
         }
         try {
-        	final CommandLine cl = parser.parse(options, commandArguments);
+            final CommandLine cl = parser.parse(options, commandArguments);
             Collection<Option> definedOptions = options.getOptions();
-            if (!definedOptions.stream().allMatch(o->cl.hasOption(o.getOpt()))) {
-            	log.error("Please specify required option.");
+            if (!definedOptions.stream().allMatch(o -> cl.hasOption(o.getOpt()))) {
+                log.error("Please specify required option.");
             }
             // default option [help]
             if (cl.hasOption("h")) {
-            	printHelp();
-            	return;
+                printHelp();
+                return;
             }
             command.run(cl);
         } catch (Throwable e) {
             // default option [help]
-            if (Arrays.asList(commandArguments).stream().anyMatch(o->o.equalsIgnoreCase("-h"))) {
-            	printHelp();
-            	return;
+            if (Arrays.asList(commandArguments).stream().anyMatch(o -> o.equalsIgnoreCase("-h"))) {
+                printHelp();
+                return;
             }
             printHelp("Can't parse arguments.");
             System.exit(1);
